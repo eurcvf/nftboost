ed843a5e986155fa027c3b08a355b1690658200d
eurcvf <eurcvf@hotmail.com>
2023-11-22 21:01:05 -0300

    Arrumado os cards

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,16 @@ import NFT02 from '@/assets/nfts/02.jpg'
 import NFT03 from '@/assets/nfts/03.jpg'
 import NFT04 from '@/assets/nfts/04.jpg'
 
+import NFT05 from '@/assets/nfts/05.jpg'
+import NFT06 from '@/assets/nfts/06.jpg'
+import NFT07 from '@/assets/nfts/07.jpg'
+import NFT08 from '@/assets/nfts/08.jpg'
+
+import NFT09 from '@/assets/nfts/09.jpg'
+import NFT10 from '@/assets/nfts/10.jpg'
+import NFT11 from '@/assets/nfts/11.jpg'
+import NFT12 from '@/assets/nfts/12.jpg'
+
 const featureCollections = [
   {
     name: 'Cat #221', 
@@ -14,56 +24,158 @@ const featureCollections = [
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
+    name: 'Snike #2331',
     thumbnail: NFT02, 
     price: '0.0721', 
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT03, 
+    name: 'Skull Hatter #2311',
+    thumbnail: NFT03,
     price: '0.0721', 
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT04, 
+    name: 'Nox #2611',
+    thumbnail: NFT04,
     price: '0.0721', 
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT01, 
-    price: '0.0721', 
+    name: 'Cat #221',
+    thumbnail: NFT01,
+    price: '0.0721',
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT02, 
-    price: '0.0721', 
+    name: 'Snike #2331',
+    thumbnail: NFT02,
+    price: '0.0721',
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT03, 
-    price: '0.0721', 
+    name: 'Skull Hatter #2311',
+    thumbnail: NFT03,
+    price: '0.0721',
     price_brl: '602,02'
   },
   {
-    name: 'Cat #221', 
-    thumbnail: NFT04, 
-    price: '0.0721', 
+    name: 'Nox #2611',
+    thumbnail: NFT04,
+    price: '0.0721',
     price_brl: '602,02'
   },
-] 
+]
+
+const featureSelections = [
+  {
+    name: 'Crazy Monkey #131',
+    thumbnail: NFT05,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Alien Shooter #2211',
+    thumbnail: NFT06,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Yordlee #1221',
+    thumbnail: NFT07,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Dragon Cannon #4401',
+    thumbnail: NFT08,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Crazy Monkey #131',
+    thumbnail: NFT05,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Alien Shooter #2211',
+    thumbnail: NFT06,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Yordlee #1221',
+    thumbnail: NFT07,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Dragon Cannon #4401',
+    thumbnail: NFT08,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+]
+
+const featureTendencies = [
+  {
+    name: 'Borex #171',
+    thumbnail: NFT10,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Durtre #2391',
+    thumbnail: NFT11,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Hatter #321',
+    thumbnail: NFT12,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Sailor Skull #4431',
+    thumbnail: NFT09,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Borex #171',
+    thumbnail: NFT10,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Durtre #2391',
+    thumbnail: NFT11,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Hatter #321',
+    thumbnail: NFT12,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+  {
+    name: 'Sailor Skull #4431',
+    thumbnail: NFT09,
+    price: '0.0721',
+    price_brl: '602,02'
+  },
+]
 
 export default function Home() {
   return (
     <>
       <SectionHero/>
       <SectionCarrousel subtitle="Destaque em Coleções" title="Coleções notáveis" data={featureCollections}/>
-      <SectionCarrousel subtitle="Seleção Mensal" title="Destaque do Mês" data={featureCollections}/>
-      <SectionCarrousel subtitle="Em Tendência" title="Tendências" data={featureCollections}/>
+      <SectionCarrousel subtitle="Seleção Mensal" title="Destaque do Mês" data={featureSelections}/>
+      <SectionCarrousel subtitle="Em Tendência" title="Tendências" data={featureTendencies}/>
     </>
   )
 }
